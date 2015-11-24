3046bbf46e538e99e1ae236e7d6981820ea5d677
shubham prasad <spd2192@gmail.com>
2015-11-24 16:05:46 +0530

    initial commit

---
+++ api/models/Player.js
@@ -0,0 +1,24 @@
+/**
+* Player.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+    name : {
+      type : "string"
+    },
+    password : {
+      type : "string"
+    },
+    current_level : {
+      type : "integer"
+    },
+    highest_score : {
+      type: "integer"
+    }
+  }
+};
