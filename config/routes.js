cb2feecc241d788745ba0b66a0820195f042b5ec
shubham prasad <spd2192@gmail.com>
2015-11-25 12:23:09 +0530

    fixed get to post in routes for /login

--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,8 @@
 
 module.exports.routes = {
 
-  "GET /login" : "PlayerController.login" ,
-  "GET /profile": "ProfileController.profile" ,
+  "POST /login" : "PlayerController.login" ,
+  "GET /profile" : "ProfileController.profile" ,
   "GET /game" : "GameController.game" ,
   "POST /game" : "GameController.judge"
 };
